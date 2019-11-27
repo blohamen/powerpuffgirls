a061840503686013a2df2731ccf3617d667d91ac
Ilya_Staver <ilya_staver@epam.com>
2019-11-27 21:04:15 +0300

    style: styling

--- src/components/containers/episode-page-container/EpisodePageContainer.js
+++ src/components/containers/episode-page-container/EpisodePageContainer.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import styles from './EpisodePageContainer.module.css';
 import excludeHtmlTags from "../../../helpers/excludeHtmlTags";
 
-export default function EpisodePageContainer({name, summary, image}) {
+export default function EpisodePageContainer({name, summary, image, season, number, runtime}) {
     return (
-        <div>
+        <div className={styles.episodePageContainer}>
             <div>
                 <h1>{name}</h1>
             </div>
             <div>
-                <p>{excludeHtmlTags(summary)}</p>
+                {season && <span>Season: {season} </span>}
+                {number && <span>Episode: {number} </span>}
+                {runtime && <span>Runtime: {runtime} </span>}
+            </div>
+            <div>
+                <p>{excludeHtmlTags(summary || '')}</p>
             </div>
             <div>
                 {image && <img src={image.medium}/>}
